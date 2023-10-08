e0fbda1285da46499baf34facdba3e616256d2a5
hongnhung25102002 <90817082+hongnhung25102002@users.noreply.github.com>
2023-10-08 23:14:46 +0700

    Giao dien trang chu, san pham, danh muc san pham

---
+++ Ictshop/Controllers/DanhmucController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ictshop.Models
+{
+    public class DanhmucController : Controller
+    {
+        // GET: Danhmuc
+        Qlbanhang db = new Qlbanhang();
+        public ActionResult Index()
+        {
+            var danhmuc = db.Hangsanxuats.ToList();
+            return PartialView(danhmuc);
+        }
+    }
+}
---
+++ Ictshop/Controllers/SanphamController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ictshop.Models
+{
+    public class SanphamController : Controller
+    {
+        // GET: Sanpham
+        //Qlbanhang db = new Qlbanhang();
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
